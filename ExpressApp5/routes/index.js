274b9479207da7a386d90dcf44b8b3de6a6bf217
Mirosław Żołądź <zoladz@agh.edu.pl>
2018-01-23 12:32:40 +0100

    git - test 2

--- ExpressApp5/routes/index.js
+++ ExpressApp5/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express - MZoladz - third commit - git hub' });
+    res.render('index', { title: 'Express - MZoladz - third commit - git hub - test2' });
 });
 
 module.exports = router;
