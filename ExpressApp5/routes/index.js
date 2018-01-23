a6f1317bab9c7026163b90bf876a973c62bfd53a
Mirosław Żołądź <zoladz@agh.edu.pl>
2018-01-23 12:22:32 +0100

    thirdt commit

--- ExpressApp5/routes/index.js
+++ ExpressApp5/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express - MZoladz' });
+    res.render('index', { title: 'Express - MZoladz - third commit' });
 });
 
 module.exports = router;
