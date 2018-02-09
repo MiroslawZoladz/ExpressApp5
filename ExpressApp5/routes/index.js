3b8d30875ab66c5722b13106befaefda1e2d81c4
Mirosław Żołądź <zoladz@agh.edu.pl>
2018-02-09 15:49:01 +0100

    a

--- ExpressApp5/routes/index.js
+++ ExpressApp5/routes/index.js
@@ -5,12 +5,12 @@ const sqlite3 = require('sqlite3').verbose();
 var router = express.Router();
 
 // open the database
-let db = new sqlite3.Database('test1.db', sqlite3.SQLITE_OPEN_CREATE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to database.');
-});
+//let db = new sqlite3.Database('test1.db', sqlite3.SQLITE_OPEN_CREATE, (err) => {
+//    if (err) {
+//        console.error(err.message);
+//    }
+//    console.log('Connected to database.');
+//});
 
 //db.serialize(() => {
 //    db.run("INSERT INTO MyTable (Name,Other) VALUES (?,?)", ['kot', 'mat']);
@@ -18,31 +18,19 @@ let db = new sqlite3.Database('test1.db', sqlite3.SQLITE_OPEN_CREATE, (err) => {
 
 //    });
 //});
-s="<table style="width:100%">
-    <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Age</th>
-    </tr>
-    <tr>
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-    </tr>
-    <tr>
-        <td>Eve</td>
-        <td>Jackson</td>
-        <td>94</td>
-    </tr>
-</table>"
+
 
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    //res.render('index', { title: 'Express - MZoladz - ble' });
-    db.all("SELECT * FROM MyTable ORDER BY ID", function (err, rows) {
-        res.json(rows); //res.console.log(rows);
-    });
+    res.render('index', { title: 'Express - MZoladz - ble ble' });
+//    db.all("SELECT * FROM MyTable ORDER BY ID", function (err, rows) {
+//        var t_row = "";
+//        for (let row of rows) {
+//            t_row = t_row + row["Time"] + "\n\r";
+//        }
+//        res.send(t_row);
+//    });
 });
 
 module.exports = router;
