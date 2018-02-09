0ec6d3950e477bb926fcc25f5c5c64f8ffdd06ff
Mirosław Żołądź <zoladz@agh.edu.pl>
2018-02-09 13:55:08 +0100

    azure new

--- ExpressApp5/routes/index.js
+++ ExpressApp5/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express - MZoladz - azure push test' });
+    res.render('index', { title: 'Express - MZoladz - azure push test new' });
 });
 
 module.exports = router;
